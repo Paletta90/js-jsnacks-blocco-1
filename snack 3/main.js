088609b5ea957dd45a225a6b723244586906b7bd
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-16 12:55:37 +0100

    Snack 3 Migliorato

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,18 +1,10 @@
-let array = [];
 let tot = 0;
 
 let container = document.getElementById("container");
 
 for (i = 0; i < 10; i++) {
 
-    array[i] = Number(prompt(`Inserisci numero ${i + 1}`));
-
-    if (!isNaN(array[i])) {
-        tot += array[i];
-    } else {
-        alert("Inserire un numero");
-    }
-    
+    tot += Number(prompt(`Inserisci numero ${i + 1}`))
 }
 
 container.innerHTML = `Totale: ${tot}`
