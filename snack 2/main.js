387b9488917c057bd04c1c8634cd9cb2d4b453a0
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-16 12:22:44 +0100

    Snack 2 (Modificato)

--- snack 2/main.js
+++ snack 2/main.js
@@ -4,9 +4,9 @@ let word2 = String(prompt("Inserisci una seconda parola"));
 let container = document.getElementById("container");
 
 if(word1.length > word2.length){
-    container.innerHTML = `La prima parola (${word1} --> lenght: ${word1.length}) è più lunga della seconda parola (${word2} --> length: ${word2.length})`
+    container.innerHTML = `Prima parola (${word1} --> lenght: ${word1.length}) <br> Seconda parola (${word2} --> length: ${word2.length})`
 }else if(word1.length < word2.length){
-    container.innerHTML = `La prima parola (${word1} --> lenght: ${word1.length}) è più corta della seconda parola (${word2} --> length: ${word2.length})`
+    container.innerHTML = `Prima parola (${word2} --> lenght: ${word2.length}) <br> Seconda parola (${word1} --> length: ${word1.length})`
 }else{
     container.innerHTML = `Le due parole hanno stessa lunghezza`
 }
